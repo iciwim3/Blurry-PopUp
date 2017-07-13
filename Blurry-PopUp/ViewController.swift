bb1965bcfc1e225e9fc8a62c8cb6974707bb7815
SKE <appybuildmore@gmail.com>
2017-07-13 13:31:45 -0500

    1. Added background image, view, and UIVisualEffect with blur to the Storyboard. 2. Created IBOutlets and IBActions and added logic to make the blur and pop up work together. 3. Photo cred: https://unsplash.com/photos/4u2U8EO9OzY?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText 4. Course cred: https://youtu.be/CXvOS6hYADc

--- Blurry-PopUp/ViewController.swift
+++ Blurry-PopUp/ViewController.swift
@@ -9,17 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var addItemView: UIView!
+    
+    @IBOutlet weak var visualEffectView: UIVisualEffectView!
+    
+    var effect: UIVisualEffect!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        effect = visualEffectView.effect
+        visualEffectView.effect = nil
+        
+        addItemView.layer.cornerRadius = 5
     }
-
+    
+    func animateIn() {
+        self.view.addSubview(addItemView)
+        addItemView.center = self.view.center
+        
+        addItemView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+        addItemView.alpha = 0
+        
+        UIView.animate(withDuration: 0.4) {
+            self.visualEffectView.effect = self.effect
+            self.addItemView.alpha = 1
+            self.addItemView.transform = CGAffineTransform.identity
+        }
+    }
+    
+    func animateOut() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.addItemView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+            self.addItemView.alpha = 0
+            
+            self.visualEffectView.effect = nil
+        }) { (success: Bool) in
+            self.addItemView.removeFromSuperview()
+        }
+    }
+    
+    @IBAction func addItem(_ sender: Any) {
+        animateIn()
+    }
+    
+    @IBAction func dismissPopUp(_ sender: Any) {
+        animateOut()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
