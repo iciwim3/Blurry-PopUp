09c3e8098768dbe6ca22be2b82ac272c02388c4e
SKE <appybuildmore@gmail.com>
2017-07-13 16:06:39 -0500

    1. Added some comments for future use and a better understanding of what is going on.

--- Blurry-PopUp/ViewController.swift
+++ Blurry-PopUp/ViewController.swift
@@ -10,26 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // IBOutlet for pop window
     @IBOutlet var addItemView: UIView!
     
+    // IBOutlet for blurred visual effect
     @IBOutlet weak var visualEffectView: UIVisualEffectView!
     
+    // Property to be able to remove or add the visual effect later on
     var effect: UIVisualEffect!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        /* Giving the View Controller access to control
+         the blurred effect. */
         effect = visualEffectView.effect
+        
+        /* Removing the blurred effect screen once the
+         app loads. */
         visualEffectView.effect = nil
         
+        // Giving the 'addItemView' rounded corners
         addItemView.layer.cornerRadius = 5
     }
     
     func animateIn() {
+        // Brings the 'addItemView' to the front of the view
         self.view.addSubview(addItemView)
+        
+        // Makes the 'addItemView' the center of the view
         addItemView.center = self.view.center
         
+        /* Makes the 'addItemView' larger by multiplying its
+         current size by 1.3 */
         addItemView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+        
+        // Makes the 'addItemView' invisible
         addItemView.alpha = 0
         
         UIView.animate(withDuration: 0.4) {
